b2786ca4eb9c673c0347e9ee69842974d99e49a9
NikkiNick <nick.gysels.w2819@student.hogent.be>
2020-01-28 18:54:27 +0100

    Add Material

---
+++ school-app/src/app/splitsmolens/splitsmolens-start/splitsmolens-start.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-splitsmolens-start',
+  templateUrl: './splitsmolens-start.component.html',
+  styleUrls: ['./splitsmolens-start.component.css']
+})
+export class SplitsmolensStartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
